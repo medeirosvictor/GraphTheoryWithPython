b75d1aa49c11fa0ce5a34c767273ae2c26daaea4
Victor Medeiros <victoramedeiros@gmail.com>
2017-10-22 15:50:53 -0300

    Starting to write in some docstrings

---
+++ functions/matrix_to_list.py
@@ -0,0 +1,5 @@
+def matrix_to_list(adj_matrix):
+    """Converts Adjacency Matrix of graph to it's Adjacency List"""
+
+    adj_list = {}
+    return adj_list
--- functions/read_input.py
+++ functions/read_input.py
@@ -1,13 +1,15 @@
 def read_input():
+    """Reads input from .txt file and returns graph_size, adj_matrix and adj_list"""
+
     import os
 
     file_path = input('Enter path to sample file...')
     assert os.path.exists(file_path), "File not found at, " + str(file_path)
 
-    AdjMatrix = []
+    adj_matrix = []
     with open(file_path, 'r') as sample_file:
         graph_size = sample_file.readline()
         for line in sample_file:
             matrix_row = list(map(int, line.split()))
-            AdjMatrix.append(matrix_row)
-    return graph_size, AdjMatrix
+            adj_matrix.append(matrix_row)
+    return graph_size, adj_matrix
