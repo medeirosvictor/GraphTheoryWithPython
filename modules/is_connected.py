7ffba5b4715802920e1a50ecdf4ea77538aca3cf
Aluno M33 <alunom33@acad.unifor.br>
2017-11-23 17:53:31 -0300

    Examples

--- modules/is_connected.py
+++ modules/is_connected.py
@@ -2,3 +2,5 @@ def is_connected(graph):
     for node in graph:
         if len(graph[node]) == 0:
             print("The graph is not connected.")
+            input("exit")
+            return False
