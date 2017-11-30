104aba6071745b491c3ca213b21e5af8f8c69bcc
Victor Medeiros <victoramedeiros@gmail.com>
2017-11-30 02:53:13 -0300

    refactor

--- MST - Not Greedy/dfs.py
+++ MST - Not Greedy/dfs.py
@@ -2,18 +2,18 @@
 
 
 def dfs_cycle(mst, start):
-    """Modified DFS to check for cycles"""
+
     colors = {node: "WHITE" for node in mst}
     colors[start] = "GRAY"
-    stack = [(None, start)]  # store edge, but at this point we have not visited one
+    stack = [(None, start)]
     while stack:
-        (prev, node) = stack.pop()  # get stored edge
+        (prev, node) = stack.pop()
         for neighbor in mst[node]:
             if neighbor == prev:
-                pass  # don't travel back along the same edge we came from
+                pass
             elif colors[neighbor] == "GRAY":
                 return True
-            else:  # can't be anything else than WHITE...
+            else:
                 colors[neighbor] = "GRAY"
-                stack.append((node, neighbor))  # push edge on stack
+                stack.append((node, neighbor))
     return False
--- MST - Not Greedy/mst_alt.py
+++ MST - Not Greedy/mst_alt.py
@@ -4,20 +4,21 @@ import operator
 from graph import Graph
 
 def mst_alt(graph, edges_weights):
-    mst = {k: [] for k in graph}
-    temp_tree = {}
+    temp_tree = {k: [] for k in graph}
+    mst = {}
     print("Edge weights: ", edges_weights)
     for edge in edges_weights:
         print("Inserted edge: ", edge)
-        temp_tree[edge] = edges_weights[edge]
-        mst[edge[0]].append(edge[1])
-        mst[edge[1]].append(edge[0])
-        if dfs_cycle(mst, 0):
-            max_key = max(temp_tree.items(), key=operator.itemgetter(1))[0]
-            print("Removed edge: ", temp_tree[max_key])
-            mst[max_key[0]].remove(max_key[1])
-            mst[max_key[1]].remove(max_key[0])
-            temp_tree.pop(max_key)
+        mst[edge] = edges_weights[edge]
+        temp_tree[edge[0]].append(edge[1])
+        temp_tree[edge[1]].append(edge[0])
+        if dfs_cycle(temp_tree, 0):
+            max_key = max(mst.items(), key=operator.itemgetter(1))[0]
+            print("Removed edge: ", max_key)
+            temp_tree[max_key[0]].remove(max_key[1])
+            temp_tree[max_key[1]].remove(max_key[0])
+            mst.pop(max_key)
         print("Partial MST: ", mst)
-    print("Final MST: ", temp_tree)
+    print("Final MST: ", mst)
+    print("Peso total da MST obtida: ", sum(mst.values()))
     return mst
